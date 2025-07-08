8a286e3dea7f1eeb4389946f31ee950851f09ba2
zhakypovmaksatbek <zh.maksatbek@gmail.com>
2025-07-08 21:26:06 +0600

    Refactor splash page layout and animations, replacing static route call with a fade-in effect. Updated branding text and removed unused code for improved clarity and performance.

--- lib/presentation/pages/splash/splash_page.dart
+++ lib/presentation/pages/splash/splash_page.dart
@@ -1,12 +1,11 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:dent_app_mobile/core/data/app_data_service.dart';
-import 'package:dent_app_mobile/generated/locale_keys.g.dart';
 import 'package:dent_app_mobile/main.dart';
 import 'package:dent_app_mobile/presentation/constants/asset_constants.dart';
 import 'package:dent_app_mobile/presentation/theme/colors/color_constants.dart';
 import 'package:dent_app_mobile/presentation/widgets/text/app_text.dart';
 import 'package:dent_app_mobile/router/app_router.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage(name: 'SplashRoute')
@@ -22,7 +21,7 @@ class _SplashPageState extends State<SplashPage> {
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 1), () {
-      route();
+      // route();
     });
   }
 
@@ -77,91 +76,97 @@ class _SplashPageState extends State<SplashPage> {
                 ),
               ),
             ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 130,
-                          height: 130,
-                          decoration: BoxDecoration(
-                            color: const Color(0xFF6770CC),
-                            // color: Colors.white,
-                            shape: BoxShape.circle,
-                            boxShadow: [
-                              BoxShadow(
-                                // color:
-                                //     const Color(0xFF6770CC).withOpacity(0.3),
-                                color: const Color.fromARGB(
-                                  255,
-                                  255,
-                                  255,
-                                  255,
-                                ).withValues(alpha: .4),
-                                blurRadius: 30,
-                                spreadRadius: 5,
+            FadeIn(
+              duration: const Duration(seconds: 3),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            width: 130,
+                            height: 130,
+                            decoration: BoxDecoration(
+                              // color: const Color(0xFF6770CC),
+                              // // color: Colors.white,
+                              // shape: BoxShape.circle,
+                              // boxShadow: [
+                              //   BoxShadow(
+                              //     // color:
+                              //     //     const Color(0xFF6770CC).withOpacity(0.3),
+                              //     color: const Color.fromARGB(
+                              //       255,
+                              //       255,
+                              //       255,
+                              //       255,
+                              //     ).withValues(alpha: .4),
+                              //     blurRadius: 30,
+                              //     spreadRadius: 5,
+                              //   ),
+                              // ],
+                            ),
+                            child: Center(
+                              child: Image.asset(
+                                AssetConstants.toothLogo.png,
+                                width: 120,
+                                height: 120,
+                                fit: BoxFit.contain,
+                                color: ColorConstants.white,
                               ),
-                            ],
+                            ),
                           ),
-                          child: Center(
-                            child: Image.asset(
-                              AssetConstants.toothLogo.png,
-                              width: 90,
-                              height: 90,
-                              fit: BoxFit.contain,
+                          SizedBox(height: 30),
+                          Text(
+                            "DentApp",
+                            style: TextStyle(
+                              fontSize: 34,
+                              fontWeight: FontWeight.bold,
                               color: ColorConstants.white,
                             ),
                           ),
-                        ),
-                        SizedBox(height: 30),
+                          SizedBox(height: 15),
+                          // Container(
+                          //   padding: EdgeInsets.symmetric(
+                          //     horizontal: 20,
+                          //     vertical: 10,
+                          //   ),
+                          //   decoration: BoxDecoration(
+                          //     color: ColorConstants.white.withValues(alpha: .1),
+                          //     borderRadius: BorderRadius.circular(30),
+                          //   ),
+                          //   child: AppText(
+                          //     title: LocaleKeys.general_splash.tr(),
+                          //     textType: TextType.body,
+                          //     color: ColorConstants.white,
+                          //   ),
+                          // ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 40),
+                    child: Column(
+                      children: [
                         AppText(
-                          title: "DentApp Mobile",
-                          textType: TextType.title,
+                          title: "from",
+                          textType: TextType.body,
                           color: ColorConstants.white,
                         ),
-                        SizedBox(height: 15),
-                        Container(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 10,
-                          ),
-                          decoration: BoxDecoration(
-                            color: ColorConstants.white.withValues(alpha: .1),
-                            borderRadius: BorderRadius.circular(30),
-                          ),
-                          child: AppText(
-                            title: LocaleKeys.general_splash.tr(),
-                            textType: TextType.body,
-                            color: ColorConstants.white,
-                          ),
+                        SizedBox(height: 5),
+                        AppText(
+                          title: "Aksoft",
+                          textType: TextType.title,
+                          color: ColorConstants.white,
                         ),
                       ],
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 40),
-                  child: Column(
-                    children: [
-                      AppText(
-                        title: "from",
-                        textType: TextType.body,
-                        color: ColorConstants.white,
-                      ),
-                      SizedBox(height: 5),
-                      AppText(
-                        title: "Aksoft",
-                        textType: TextType.body,
-                        color: ColorConstants.white,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
           ],
         ),
