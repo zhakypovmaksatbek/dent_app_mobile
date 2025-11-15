416bb7a24b7b560ced12224c1dc1768ebd8e8a6a
zhakypovmaksatbek <zh.maksatbek@gmail.com>
2025-11-15 19:15:11 +0600

    Update NDK version to 28.2.13676358 and increment app version to 1.6.3+31

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ if (keystorePropertiesFile.exists()) {
 android {
     namespace = "com.aksoft.dentapp_aksoft"
     compileSdk = 36
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
